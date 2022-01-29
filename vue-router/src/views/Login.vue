5ad1fb6acc80d94c31f13411ead710beb69235cb
booding <booding.xiu@outlook.com>
2022-01-29 22:01:43 +0800

    add: add dynamic router

---
+++ vue-router/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Login Page
+    <br>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
